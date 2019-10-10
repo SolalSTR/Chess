8da18c46391462250188e7daf017aad1e3357234
SolalSTR <sstradella@gmail.com>
2019-10-10 10:52:38 +0200

    plateau

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import Plateau from './elements/Plateau';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Plateau size={10} firstColor="#323232" secondaryColor="#cfcfcf"/>
   );
 }
-
-export default App;
