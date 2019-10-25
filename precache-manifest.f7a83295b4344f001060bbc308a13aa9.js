18363cfe828b3237009215524402a553f58d1adc
SolalSTR <sstradella@gmail.com>
2019-10-25 14:53:30 +0200

    Updates

---
+++ precache-manifest.f7a83295b4344f001060bbc308a13aa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810395a6fdbb5b3dc9cb274ad41bc780",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "ba78c0eb70b842a92bbc",
+    "url": "/chess/static/css/main.57509a25.chunk.css"
+  },
+  {
+    "revision": "64b4bf188f4c5b3399ce",
+    "url": "/chess/static/js/2.0a47f0ad.chunk.js"
+  },
+  {
+    "revision": "ba78c0eb70b842a92bbc",
+    "url": "/chess/static/js/main.22315e2b.chunk.js"
+  },
+  {
+    "revision": "d66fbe0136305279c7ad",
+    "url": "/chess/static/js/runtime-main.fbb22cc1.js"
+  }
+]);
